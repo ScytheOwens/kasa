278b64e9d5d3a6eeb8d17d39c62a4a305c2308a0
Tail_Straw <maeva.gr@live.fr>
2024-05-28 00:21:41 +0200

    Project initialisation

---
+++ src/pages/Home/Banner.jsx
@@ -0,0 +1,9 @@
+import '../styles/banner.css';
+
+function Banner() {
+    return (
+        <h1 className="header-title">La maison jungle</h1>
+    );
+}
+
+export default Banner;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import '../../App.css';
+
+function App() {
+  return (
+    <h1>Test</h1>
+  )
+}
+
+export default App;
