b4c10b9da8b7918b6a13992d914ff1b3d7e63047
Tail_Straw <maeva.gr@live.fr>
2024-05-29 00:02:18 +0200

    Add routing, 404 management and basic style

---
+++ src/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/home';
+import About from './pages/About/about';
+import Product from './pages/Product/product';
+import Error from './components/Error';
+import './App.scss';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Header/>
+      <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/a-propos" element={<About/>}/>
+          <Route path="/hebergement" element={<Product/>}/>
+          <Route path="*" element={<Error />}/>
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
