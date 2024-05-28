36c94042fbb6ae105c80e9c5e6d2137983e5a64a
Tail_Straw <maeva.gr@live.fr>
2024-05-28 18:11:17 +0200

    Add basic app structure

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function About() {
+    return (
+        <h1>Page A propos</h1>
+    )
+}
+
+export default About;
--- src/pages/Home/Banner.jsx
+++
@@ -1,9 +0,0 @@
-import '../styles/banner.css';
-
-function Banner() {
-    return (
-        <h1 className="header-title">La maison jungle</h1>
-    );
-}
-
-export default Banner;
