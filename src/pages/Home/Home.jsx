a3300f7ce799efe6fdf5dd20d76ff689d2de0fea
Tail_Straw <maeva.gr@live.fr>
2024-05-30 23:31:32 +0200

    Add home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import Banner from '../../components/Banner';
+import Gallery from '../../components/Gallery';
+import Card from '../../components/Card';
+import bannerMedia from '../../assets/images/shore.png';
+import accomodations from '../../var/accomodations.json';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <h1>Test</h1>
+    <React.Fragment>
+      <Banner
+        title="Chez vous, partout et ailleurs"
+        mediaUrl={ bannerMedia }
+        mediaDescription="Photographie de falaises"
+      />
+      <Gallery
+        items={accomodations.map(item =>
+          <Card
+            key={ item.id }
+            title={ item.title }
+            slug={ item.title }
+            mediaUrl={ item.cover }
+            mediaDescription={ item.title }
+          />
+        )}
+      />
+    </React.Fragment>
   )
 }
 
