4d70cd714b461b90ce6d82a5a14decfad635d08a
Tail_Straw <maeva.gr@live.fr>
2024-05-30 23:45:24 +0200

    Add banner to about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Banner from "../../components/Banner";
+import bannerMedia from "../../assets/images/mountains.png";
 
 function About() {
     return (
-        <h1>Page A propos</h1>
+        <React.Fragment>
+            <Banner
+                mediaUrl={ bannerMedia }
+                mediaDescription="Photographie de montagnes"
+            />
+        </React.Fragment>
     )
 }
 
