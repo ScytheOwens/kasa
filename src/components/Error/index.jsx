9d6334893aefe924f2767985abfb18ae6c961aee
Tail_Straw <maeva.gr@live.fr>
2024-05-30 23:24:45 +0200

    Add error component

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Error() {
     return (
-        <h1>Erreur 404</h1>
+        <section className="error">
+            <h1 className="error-title">404</h1>
+            <p className="error-text">Oups ! La page que vous demandez n'existe pas.</p>
+            <Link to="/" className="error-link">Retourner sur la page d'accueil</Link>
+        </section>
     )
 }
  
