cf3184b614cc75ba222c2998662fcfbb7f5f960b
Tail_Straw <maeva.gr@live.fr>
2024-06-02 23:13:15 +0200

    Add drop menu component and style, and integrate on about page

---
+++ src/components/Dropbox/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import chevronIcon from "../../assets/icons/chevron-up.svg";
+
+function Dropbox({ items }) {
+    console.log(items);
+
+    return(
+        <ul className="dropbox">
+            {items.map((item, title) =>
+                <li className="dropbox-item" key={"dropbox-item-" + title}>
+                    <input type="checkbox" id={"dropbox-item-" + item.title} className="dropbox-item-input"/>
+                    <h2 className="dropbox-item-title">
+                        <label for={"dropbox-item-" + item.title} className="dropbox-item-title-label">{ item.title }<img src={ chevronIcon } className="dropbox-item-title-label-icon"/></label>
+                    </h2>
+                    <p className="dropbox-item-content">{ item.content }</p>
+                </li>
+            )}
+        </ul>
+    )
+}
+
+export default Dropbox;
