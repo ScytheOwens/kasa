7e275971a7beed45391d974a1b124b8507b6c374
Tail_Straw <maeva.gr@live.fr>
2024-06-03 18:47:32 +0200

    Move policy into json to split template and data

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,27 +2,9 @@ import React from "react";
 import Banner from "../../components/Banner";
 import Dropbox from "../../components/Dropbox";
 import bannerMedia from "../../assets/images/mountains.png";
+import policy from "../../var/policy.json";
 
 function About() {
-    const itemArray=[
-        {
-            "title": "Fiabilité",
-            "content":"Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
-        },
-        {
-            "title": "Respect",
-            "content": "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-        },
-        {
-            "title": "Service",
-            "content": "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-        },
-        {
-            "title": "Sécurité",
-            "content": "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
-        }
-    ];
-
     return (
         <React.Fragment>
             <Banner
@@ -31,7 +13,7 @@ function About() {
             />
 
             <Dropbox
-                items={ itemArray }
+                items={ policy }
             />
         </React.Fragment>
     )
