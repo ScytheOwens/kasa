30bb373f1e517084ad5d40ccc5ba10c21f87e232
Tail_Straw <maeva.gr@live.fr>
2024-06-07 19:52:21 +0200

    Add accomodation presentation

--- src/components/Dropbox/index.jsx
+++ src/components/Dropbox/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import chevronIcon from "../../assets/icons/chevron-up.svg";
+import PropTypes from "prop-types";
 
 function Dropbox({ items }) {
     return(
@@ -10,11 +11,24 @@ function Dropbox({ items }) {
                     <h2 className="dropbox-item-title">
                         <label htmlFor={"dropbox-item-" + item.title} className="dropbox-item-title-label">{ item.title }<img src={ chevronIcon } className="dropbox-item-title-label-icon"/></label>
                     </h2>
-                    <p className="dropbox-item-content">{ item.content }</p>
+                    <p className="dropbox-item-content">
+                        { Array.isArray(item.content) ?
+                            <ul>
+                                { item.content.map((paragraph, index) =>
+                                    <li key={"paragraph-" + index}>{paragraph}</li>
+                                )}
+                            </ul>
+                            : item.content
+                        }
+                    </p>
                 </li>
             )}
         </ul>
     )
+}
+
+Dropbox.Prototypes = {
+    items: PropTypes.array.isRequired
 }
 
 export default Dropbox;
