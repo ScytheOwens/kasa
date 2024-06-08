05a76ee93a1b4e5de178de2fc982f9a0cea95ed6
Tail_Straw <maeva.gr@live.fr>
2024-06-08 03:13:47 +0200

    Fix 404 routing for products and some design

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import Error from "../../components/Error";
 import Slideshow from "../../components/Slideshow";
 import Avatar from "../../components/Avatar";
@@ -75,7 +75,7 @@ function Product() {
         )
     } else {
         return (
-            <Error/>
+            <Navigate to="*"/>
         )
     }
 }
